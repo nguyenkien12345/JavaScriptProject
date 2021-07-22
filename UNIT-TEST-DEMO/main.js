8a632611bc25e728910e645497393b38ccea5d94
nguyenkien <nguyenkien11202000@gmail.com>
2021-07-22 21:51:16 +0700

    Upload All Projcect

--- UNIT-TEST-DEMO/main.js
+++ UNIT-TEST-DEMO/main.js
@@ -1,10 +1,45 @@
-// Pure functions (Chức năng thuần tuý)
-
 // Hàm kiểm tra số lẻ
-export const isOddNumber = (number) => number % 2 === 1;
+export const isOddNumber = (number) => number % 2 !== 0;
+// Hàm kiểm tra số chẵn
+export const isEvenNumber = (number) => number % 2 === 0;
 
+
+// Hàm đếm số lẻ
+export const countOddNumbers = (numberList) => {
+    if(!Array.isArray(numberList)) return 0;
+    return numberList.filter((x) => x % 2 !== 0).length;
+};
 // Hàm đếm số chẵn
 export const countEvenNumbers = (numberList) => {
     if(!Array.isArray(numberList)) return 0;
     return numberList.filter((x) => x % 2 === 0).length;
 };
+
+
+export class Caculator{
+    // Hàm cộng
+    Summation = (a, b) => { return a + b; }
+    // Hàm trừ
+    Subtraction = (a, b) => { return a - b; }
+    // Hàm nhân
+    Multiplication = (a, b) => { return a * b; }
+    // Hàm chia
+    Division = (a, b) => { return a / b; }
+}
+
+// Hàm kiểm tra năm nhuận
+export const isLeapYear = (year) => {
+    if(year % 400 === 0) return true;
+    if(year % 4 === 0) return true;
+    if(year % 100 === 0) return false; 
+    return false;
+}
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------
+// Phân biệt Black Box và White Box
+
+// - Black Box Testing tức là các bạn thấy 1 chương trình nhưng các bạn không biết chương trình đó hoạt động như thế nào cả, chỉ biết đưa đầu 
+//   vào như thế này đầu ra như thế kia. Nó như 1 cái hộp đen không biết bên trong có gì cả
+
+// - White Box Testing tức là các bạn biết chương trình chạy như thế nào và test xem nó đúng hay sai
--- UNIT-TEST-DEMO/main.test.js
+++ UNIT-TEST-DEMO/main.test.js
@@ -1,7 +1,7 @@
-import { isOddNumber,countEvenNumbers } from './main.js';
+import { isOddNumber, isEvenNumber, countOddNumbers ,countEvenNumbers, Caculator, isLeapYear } from './main.js';
 
 const expect = chai.expect;
-
+// --------------------------------------------------------------------------------------------------------------------------------------
 describe('function isOddNumber', () => {
     it('should be true if 3', () => {
         const result = isOddNumber(3);
@@ -13,7 +13,19 @@ describe('function isOddNumber', () => {
         expect(result).to.be.false;
     });
 })
+// --------------------------------------------------------------------------------------------------------------------------------------
+describe('function isEvenNumber', () => {
+    it('should be true if 4', () => {
+        const result = isEvenNumber(4);
+        expect(result).to.be.true;
+    });
 
+    it('should be false if 3', () => {
+        const result = isEvenNumber(3);
+        expect(result).to.be.false;
+    });
+})
+// --------------------------------------------------------------------------------------------------------------------------------------
 describe('function countEvenNumbers', () => {
     it('should be 0 if null', () => {
         const result = countEvenNumbers(null);
@@ -32,3 +44,77 @@ describe('function countEvenNumbers', () => {
         expect(result).to.equal(2);
     });
 })
+// --------------------------------------------------------------------------------------------------------------------------------------
+describe('function countOddNumbers', () => {
+    it('should be 0 if null', () => {
+        const result = countOddNumbers(null);
+        expect(result).to.equal(0);
+    });
+    it('should be 0 if []', () => {
+        const result = countOddNumbers([]);
+        expect(result).to.equal(0);
+    });
+    it('should equal 1 if [1,2,3]', () => {
+        const result = countOddNumbers([1,2,3]);
+        expect(result).to.equal(2);
+    });
+    it('should equal 2 if [1,2,3,4]', () => {
+        const result = countOddNumbers([1,2,3,4]);
+        expect(result).to.equal(2);
+    });
+})
+// --------------------------------------------------------------------------------------------------------------------------------------
+describe('Caculator', () => {
+    it('The result should be 30 if you do the addition between 10 and 20', () => {
+        // Set up
+        const caculator_add = new Caculator();
+        // Run
+        const result = caculator_add.Summation(10, 20);
+        // Assert
+        expect(result).to.equal(30);
+    });
+    it('The result should be 50 if you do the addition between 100 and 50', () => {
+        // Set up
+        const caculator_sub = new Caculator();
+        // Run
+        const result = caculator_sub.Subtraction(100, 50);
+        // Assert
+        expect(result).to.equal(50);
+    });
+    it('The result should be 5000 if you do the addition between 100 and 50', () => {
+        // Set up
+        const caculator_mul = new Caculator();
+        // Run
+        const result = caculator_mul.Multiplication(100, 50);
+        // Assert
+        expect(result).to.equal(5000);
+    });
+    it('The result should be 22 if you do the addition between 44 and 2', () => {
+        // Set up
+        const caculator_div = new Caculator();
+        // Run
+        const result = caculator_div.Division(44, 2);
+        // Assert
+        expect(result).to.equal(22);
+    });
+})
+// --------------------------------------------------------------------------------------------------------------------------------------
+describe('function isLeapYear', () => {
+    it('should be false if Leap Year is 1990', () => {
+        const result = isLeapYear(1990);
+        expect(result).to.be.false;
+    });
+    it('should be false if Leap Year is 2003', () => {
+        const result = isLeapYear(2003);
+        expect(result).to.be.false;
+    });
+    it('should be true if Leap Year is 400', () => {
+        const result = isLeapYear(400);
+        expect(result).to.be.true;
+    });
+    it('should be true if Leap Year is 2000', () => {
+        const result = isLeapYear(2000);
+        expect(result).to.be.true;
+    });
+})
+
